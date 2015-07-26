ad625e4d87c93f69ac3fa4930a057df9bd00e1e9
Uli Köhler <ulikoehler@online.de>
2015-07-26 06:11:04 +0200

    Fix report

--- utils/report.php
+++ utils/report.php
@@ -1,12 +1,18 @@
 <?php
 $to      = 'ukoehler@techoverflow.net';
-$subject = '[KATranslationCheck-Report] ' + $_POST["reason"];
-$message = "URL: $_POST['url']\n\nmsgid: $_POST['msgid']\n\n$_POST['msgstr']\n\nHit: $_POST['hit']\n\nFilename: $_POST['filename']";
+$subject = '[KATranslationCheck-Report] ' . $_POST["reason"];
+$url = $_POST['url'];
+$msgid = $_POST['msgid'];
+$msgstr = $_POST['msgstr'];
+$hit = $_POST['hit'];
+$filename = $_POST['filename'];
+$reason = $_POST['reason'];
+$message = "URL: $url\n\nmsgid: $msgid\n\nmsgstr: $msgstr\n\nHit: $hit\n\nFilename: $filename";
 $headers = 'From: webmaster@localgrid.de' . "\r\n" .
     'Reply-To: webmaster@localgrid.de' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 mail($to, $subject, $message, $headers);
 
-echo '{"status":"success"}'
+echo '{"status":"success"}';
 ?> 
