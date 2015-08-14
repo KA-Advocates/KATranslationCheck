155e5820d91962fc413d4d0377af2b3b6ddc330b
Uli Köhler <ulikoehler@online.de>
2015-08-14 19:02:45 +0200

    Fix rule unit tests

--- de.py
+++ de.py
@@ -173,7 +173,7 @@ if __name__ == "__main__":
     print("Counting %d rules" % len(rules))
     #Rule tests. python3 Rules.py to run
     assert(findRule(rules, "Comma in coordinate (| required)")("$(12,3)$", ""))
-    assert(findRule(rules, "Simple number with decimal point instead of comma")("$12.3$", ""))
+    assert(findRule(rules, "Decimal point instead of comma")("$12.3$", ""))
     assert(findRule(rules, "Value with embedded dollar symbol")("$\\\\$12$", ""))
     assert(findRule(rules, "Value with embedded dollar symbol")("$\\\\$12.5$", ""))
     assert(findRule(rules, "Value with embedded dollar symbol")("$\\\\$12,5$", ""))
