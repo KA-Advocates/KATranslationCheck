ed4d6e39d50eb46fc4bfc621ade50a63fd0e9135
Uli Köhler <ulikoehler@online.de>
2015-10-12 03:36:41 +0200

    Added automated fetching of lint CSV

--- ReadLint.py
+++ ReadLint.py
@@ -5,6 +5,9 @@ Official Khan Academy Lint reader
 from collections import namedtuple
 import csv
 import requests
+import time
+from lxml.html import fromstring
+from selenium import webdriver
 
 LintEntry = namedtuple("LintEntry", ["date", "url", "text"])
 
@@ -14,5 +17,57 @@ def readLintCSV(filename):
         reader = csv.reader(lintin, delimiter=',')
         return [LintEntry(row[0], row[1], row[2]) for row in reader]
 
+def fetchSelenium(url):
+    browser = webdriver.PhantomJS()
+    browser.set_window_size(1920, 1080)
+    browser.get(url)
+    time.sleep(2)
+    page_source = browser.page_source
+    browser.quit()
+    return page_source
+
+def fetchSeleniumFF(url):
+    browser = webdriver.Firefox()
+    browser.set_window_size(1920, 1080)
+    browser.get(url)
+    time.sleep(2)
+    page_source = browser.page_source
+    browser.quit()
+    return page_source
+
+def getLatestLintPostURLForLanguage(lang="de"):
+    """
+    Get the latest lint result URL from google groups for a given language.
+    Returns the URL to a Google Groups post.
+    """
+    page_source = fetchSelenium("https://groups.google.com/a/khanacademy.org/forum/#!forum/i18n-reports")
+    frontpage = fromstring(page_source)
+    frontpage.make_links_absolute("https://groups.google.com/a/khanacademy.org/forum")
+    elements = frontpage.xpath('//a[@class="IVILX2C-p-Q"]') # WTF is this string?
+    #with open("out.html","w") as outf:
+    #    outf.write(page_source)
+    for element in elements:
+        txt = element.text
+        if txt.endswith("crowdin entries linted for {0}".format(lang)):
+            return (element.get("href"))
+
+def getLatestLintDownloadLinks(lang="de"):
+    page_source = fetchSeleniumFF(getLatestLintPostURLForLanguage(lang))
+    frontpage = fromstring(page_source)
+    elements = frontpage.xpath('//a[@class="gwt-Anchor"]') # WTF is this string?
+    # Return only last link
+    latest = None
+    for element in elements:
+        txt = element.text
+        if txt == "Herunterladen":
+            latest = element.get("href")
+    return latest
+
+
+def getCSV(): pass
+
 if __name__ == "__main__":
-    print(readLintCSV("de-lint.csv"))
+    #url = getLatestLintPostURLForLanguage()
+    #print(url)
+    print(getLatestLintDownloadLinks())
+    #print(readLintCSV("de-lint.csv"))
