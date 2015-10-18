70d0e4c0405abc1c59d50e38bfcd467e0cc342ff
Uli Köhler <ulikoehler@online.de>
2015-10-18 20:21:04 +0200

    Quadratic expression does not need to be translated as term

--- de.py
+++ de.py
@@ -278,7 +278,7 @@ rules = [
     ExactCopyRule("All image URLs must match in order", r"!\[\]\s*\([^\)]+\)", severity=Severity.warning, aliases=imageAliases),
     ExactCopyRule("All GUI elements must match in order", r"\[\[☃\s+[a-z-]+\s*\d*\]\]", severity=Severity.warning),
     # Unsorted stuff
-    TranslationConstraintRule("'expression' not translated to 'Term'", r"(?<!polynomial )(?<!☃ )expression", r"term", severity=Severity.notice, flags=re.UNICODE | re.IGNORECASE),
+    TranslationConstraintRule("'expression' not translated to 'Term'", r"(?<!polynomial )(?<!quadratic )(?<!☃ )expression", r"term", severity=Severity.notice, flags=re.UNICODE | re.IGNORECASE),
     TranslationConstraintRule("'polynomial expression' not translated to 'Polynom'", r"polynomial expression", r"Polynom", severity=Severity.notice, flags=re.UNICODE | re.IGNORECASE),
 ]
 
