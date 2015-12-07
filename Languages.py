ab2f4aaf9546787a269a3d0ec5b3b83c86a43bde
Uli Köhler <ulikoehler@online.de>
2015-12-07 20:51:48 +0100

    Return language set instead of language generator

--- Languages.py
+++ Languages.py
@@ -6,8 +6,10 @@ def findAllLanguages():
     "Find a list of Crowdin language codes to which KA is translated to"
     response = requests.get("https://crowdin.com/project/khanacademy")
     txt = response.text
+    langs = set()
     for match in re.findall(r"https?://[a-z0-9]*\.cloudfront\.net/images/flags/([^\.]+)\.png", txt):
-        yield match
+        langs.add(match)
+    return langs
 
 if __name__ == "__main__":
-    print(list(findAllLanguages()))
+    print(findAllLanguages())
