2bc91d9ee3ca4e47cea1400260d06e84b1f9a435
Uli Köhler <ulikoehler@online.de>
2015-12-07 03:33:31 +0100

    Added language code extractor

---
+++ Languages.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import requests
+import re
+
+def findLanguages():
+    "Find a list of Crowdin language codes to which KA is translated to"
+    response = requests.get("https://crowdin.com/project/khanacademy")
+    txt = response.text
+    for match in re.findall(r"https?://[a-z0-9]*\.cloudfront\.net/images/flags/([^\.]+)\.png", txt):
+        yield match
+
+if __name__ == "__main__":
+    print(list(findLanguages()))
