5b6960e915dab09b369fc1d440d30826191e9e4c
Uli Köhler <ulikoehler@online.de>
2015-12-09 00:08:29 +0100

    Added polyglott server prototype

---
+++ IndexProcessor/server/index.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+
+var TranslationIndex = require('../TranslationIndex.json');
+var TranslationMap = require('../TranslationMap.json');
+
+app.get('/translate.json', function (req, res) {
+    var key = req.query.s;
+    var engkey = TranslationIndex[key];
+    var result = TranslationMap[engkey];
+    //Inject english version
+    result.en = engkey;
+    res.json(result);
+})
+
+var port = 7160
+console.log("Starting server on port " + port)
+app.listen(7160)
