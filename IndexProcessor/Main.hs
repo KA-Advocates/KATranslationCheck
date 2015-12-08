263c3f34bf4b739e3b972ff86606b54de0362d68
Uli Köhler <ulikoehler@online.de>
2015-12-08 15:23:58 +0100

    IndexProcessor: Added capability of processing multiple POT directories

--- IndexProcessor/Main.hs
+++ IndexProcessor/Main.hs
@@ -7,10 +7,16 @@ import Data.ByteString.Char8 (ByteString)
 import qualified Data.Text as T
 import Data.Text (Text)
 import Data.Maybe
+import System.FilePath.Posix
 import Data.Either.Combinators
 import Control.Concurrent.Async
-
 import System.Directory.Extra (listFilesRecursive)
+import qualified Data.Map.Strict as M
+
+-- A map from language to translation, e.g. "de" -> "Polynom", "en" -> "Polynome"
+type LanguageMap = [(Text, Text)]
+-- A translation map which associates a key with a list of translations
+type TranslationMap = M.Map Text LanguageMap
 
 processPOFile :: FilePath -> IO [(Text, Text)]
 processPOFile fp = processPOData <$> B.readFile fp
@@ -33,7 +39,23 @@ processPODirectory dir = do
     files <- listFilesRecursive dir
     concat <$> mapConcurrently processPOFile files
 
+-- Process a directory of PO files
+processPODirectories :: FilePath -> [Text] -> IO [TranslationMap]
+processPODirectories dir langs = forM langs $ \lang -> do
+        let curdir = dir </> T.unpack lang
+        results <- processPODirectory curdir
+        return $ poDirResultToTranslationMap lang results
+
+poDirResultToTranslationMap :: Text -> [(Text, Text)] -> TranslationMap
+poDirResultToTranslationMap lang results =
+    let f (k, v) = (k, [(lang, v)])
+    in M.fromList $ map f results
+
+unionTranslationMap :: TranslationMap -> TranslationMap -> TranslationMap
+unionTranslationMap = M.unionWith (++)
+
 main :: IO ()
 main = do
-    results <- processPODirectory "../cache/de"
-    print $ results
+    results <- processPODirectories "../cache" ["de", "fr"]
+    print $ foldr1 unionTranslationMap results
+    
